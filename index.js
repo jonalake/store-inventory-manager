5d3d3c46a2fca0c967dd2cac3a91d5207be016a0
Jon Lake <jlake21@ford.com>
2022-04-14 20:04:52 -0600

    Add Item Input Functionality

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
-const form = document.forms.input
+const input = document.querySelector("#input")
+const status = document.querySelector("#status")
+let inventory = []
 
-function handleSubmit(event) {
+input.addEventListener("submit", (event) => {
     event.preventDefault()
-    console.log("Yay, it works")
-}
-
-form.addEventListener("submit", handleSubmit)
-
-
-
-
-
-
-/* [{
-    item_name:
-    quality:
-    sell_in:
-    date_added:
-    category:
-}, {
-    item_name:
-    quality:
-    sell_in:
-    date_added:
-    category:
-}] */
+    const formData = new FormData(event.target)
+    const item = {
+        item_name: formData.get("item"),
+        quality: formData.get("sell_in"),
+        sell_in: formData.get("quality"),
+        date_added: formData.get("date_added"),
+        category: "none",
+    }
+    if (item.item_name.includes("Aged Brie")) {
+        item.category = "Aged Brie"
+    } else if (item.item_name.includes("Sulfuras")) {
+        item.category = "Sulfuras"
+    } else if (item.item_name.includes("Backstage passes")) {
+        item.category = "Backstage passes"
+    } else if (item.item_name.includes("Conjured")) {
+        item.category = "Conjured"
+    }
+    inventory = [...inventory, item]
+    console.log(inventory)
+})
